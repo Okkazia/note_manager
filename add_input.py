2a7c313ae228bfa81c3028da312e9e856bab84f8
Okkazia <okkazia@gmail.com>
2025-01-12 20:14:18 +0300

    add_input.py

--- add_input.py
+++ add_input.py
@@ -3,11 +3,11 @@ username = input("Введите имя пользователя: ")
 title = input("Введите заголовок заметки: ")
 content = input("Введите описание заметки: ")
 status = input("Введите статус заметки: ")
-created_date = input("Введите дату создания заметки в формате: Число. Месяц. Год. ")
-issue_date = input("Введите дату истечения заметки в формате: Число. Месяц. Год. ")
+created_date = input("Введите дату создания заметки в формате: Число.Месяц.Год. ")
+issue_date = input("Введите дату истечения заметки в формате: Число.Месяц.Год. ")
 
-temp_issue_date = created_date[0:7]
-temp_created_date = issue_date[0:7]
+temp_issue_date = created_date[0:6]
+temp_created_date = issue_date[0:6]
 
 print("Имя пользователя:", username)
 print("Заголовок заметки:", title)
