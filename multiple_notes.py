df22bfdcfd194d5e7300eee1ae691d1712a8a5fe
Okkazia <okkazia@gmail.com>
2025-01-27 21:22:15 +0300

    multiple_notes.py

--- multiple_notes.py
+++ multiple_notes.py
@@ -1,33 +1,46 @@
 
-list_notes = []
-def create_note(** values):
-    global list_notes
+from datetime import date
+
+list_notes = []  # Создаем список для хранения словарей-заметок
+
+def create_note(** values):  # Создаем функцию, добавляющую словари в список
     list_notes.append(values)
 
 print("Добро пожаловать в 'Менеджер заметок'! Вы можете добавить новую заметку.")
+
 while True:
-    ans = input("Хотите добавить заметку?(Да/Нет):")
+    ans = (input(  # Запрашиваем ввод от пользователя и корректируем его
+        "Хотите добавить заметку?(Да/Нет):").
+           strip().capitalize())
     if ans == "Да":
-        create_note(
-            username=input("Введите имя пользователя: "),
-            title=input("Введите заголовок заметки: "),
-            content=input("Введите описание заметки: "),
-            status=input("Введите статус заметки: "),
-            created_date=input("Введите дату создания заметки в формате: Число.Месяц.Год. "),
-            issue_date=input("Введите дату истечения заметки в формате: Число.Месяц.Год. "))
+        note = {  # Создание новой заметки
+            'username': input("Введите имя пользователя: "),
+            'title': input("Введите заголовок заметки: "),
+            'content': input("Введите описание заметки: "),
+            'status': input("Введите статус заметки: "),
+            'created_date': input("Введите дату создания заметки в формате: Год-Месяц-Число. "),
+            'issue_date': input("Введите дату истечения заметки в формате: Год-Месяц-Число. ")}
+        try:  # Проверяем формат ввода даты
+            created_date_ = date.fromisoformat(note['created_date'])
+            deadline_date = date.fromisoformat(note['issue_date'])
+            note['created_date'] = created_date_
+            note['issue_date'] = deadline_date
+        except ValueError:
+            print("Некорректный формат даты. Попробуйте еще раз.")
+            continue
+
+        create_note(**note)  # Вызываем функцию и добавляем заметку в список
         print("Заметка добавлена! Можно добавить еще.")
-    elif ans == "Нет":
+    elif ans == "Нет":  # Оканчиваем добавление заметок
         break
-    else:
+    else:  # Исправляем пользователя
         print("Некорректный ввод. Попробуйте еще раз.")
 
 
-for i in list_notes:
+for i in list_notes: # Вывод всех заметок
     print("\n"'Заголовок заметки:', i["title"])
     print('Имя пользователя:', i["username"])
     print('Описание заметки:', i["content"])
     print('Статус заметки:', i["status"])
     print('Дата создания заметки:', i["created_date"])
     print('Дата истечения заметки:', i["issue_date"],)
-
-
